78afc1f1a16294df90a6f3d719d3fe67e7cd03e1
Sue Uland <SueUland@comcast.net>
2024-04-03 07:37:05 -0700

    Initial Commit

--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,51 @@
-public class Person {
-    private double height;
+public class Person implements Comparable<Person> {
+    // instance fields
+    private String name; // null
+    private int age; //0
 
-    public Person(double height) {
-        this.height = height;
+    // constructor - parameterized
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public double getHeight() {
-        return height;
+    // order by age in increasing order
+    public int compareTo(Person other) {
+        /*
+        // this person goes after other person
+        if (this.age > other.age) {
+            return 1;
+        }
+
+        if (this.age < other.age) {
+            return -1;
+        }
+
+        // if(this.age == other.age){
+        return 0;
+        */
+
+        //  return this.age - other.age;
+        // sort by name instead
+
+        //return this.name.compareTo(other.name);
+        // sort by name, then if names are equal sort by ages
+        if(this.name.compareTo(other.name) == 0){
+            // compare the ages
+            return this.age - other.age;
+        }
+        return name.compareTo(other.name);
+
+    }
+    // toString()
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
+
--- src/PersonTester.java
+++ src/PersonTester.java
@@ -1,2 +1,24 @@
+import java.util.*;
+
 public class PersonTester {
+    public static void main(String[] args) {
+        Person bob = new Person("Bob", 25);
+        Person jose = new Person("Jose", 18);
+        Person abby = new Person("Abby", 65);
+        Person abby2 = new Person("Abby", 53);
+        // bob is this, jose is other
+        int result = bob.compareTo(jose);
+        System.out.println(result);
+
+        // construct an ArrayList of Person type and add the objects
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(bob);
+        list.add(jose);
+        list.add(abby);
+        list.add(abby2);
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
 }
